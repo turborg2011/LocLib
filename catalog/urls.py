a96c54d881e68dffd7020534229e107c784eae3a
Haydar <turborg2011@gmail.com>
2022-08-21 00:03:04 +0700

    сделал страницу подробного вывода книг и страницу списка книг, также добавил пагинацию

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.urls import path, re_path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
-
 
--- catalog/views.py
+++ catalog/views.py
@@ -17,12 +17,22 @@ def index(request):
     #тут либо object либо objects
     num_authors = Author.objects.count()
 
+    num_books_found_edge = Book.objects.all().filter(title="Foundation's Edge").count()
+
     #Render HTML with given data inside variable 'context'
     return render(
         request, 'index.html',
         context={'num_books':num_books, 'num_instances':num_instances,
-                 'num_instances_available':num_instances_available, 'num_authors': num_authors}
+                 'num_instances_available':num_instances_available, 'num_authors': num_authors,
+                 'num_books_found_edge':num_books_found_edge}
     )
 
 
+from django.views import generic
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
